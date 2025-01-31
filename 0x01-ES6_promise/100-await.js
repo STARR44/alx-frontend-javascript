66adb519600708aa8c4b52780e6125a0b44d4bb5
STARR44 <okekeesther100@gmail.com>
2025-01-31 12:45:01 +0100

    feat: Implement asyncUploadUser function with async/await

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let response = {};
+
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    response = { photo, user };
+  } catch (error) {
+    response = { photo: null, user: null };
+  }
+
+  return response;
+}
