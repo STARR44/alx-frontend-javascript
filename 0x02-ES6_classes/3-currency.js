d9c83b1f66d3b8940d39eee52b7ea40188f57803
STARR44 <okekeesther100@gmail.com>
2025-02-01 08:14:56 +0100

    feat: implement Currency class with getters, setters, and displayFullCurrency method

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,31 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  displayFullCurrency() {
+    // eslint-disable-next-line no-underscore-dangle
+    return `${this._name} (${this._code})`;
+  }
+
+  get code() {
+    // eslint-disable-next-line no-underscore-dangle
+    return this._code;
+  }
+
+  set code(value) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._code = value;
+  }
+
+  get name() {
+    // eslint-disable-next-line no-underscore-dangle
+    return this._name;
+  }
+
+  set name(value) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._name = value;
+  }
+}
