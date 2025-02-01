dc935b95e3cdef81c4658af1c8b268a344e5f334
STARR44 <okekeesther100@gmail.com>
2025-02-01 08:17:16 +0100

    feat: add abstract Building class with sqft getter and method enforcement

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,14 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    // eslint-disable-next-line no-underscore-dangle
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    // eslint-disable-next-line no-underscore-dangle
+    return this._sqft;
+  }
+}
